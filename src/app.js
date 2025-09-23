e217f9428ebaaa0c383d6237541d3dbc84a16eb1
Lokesh Kandpal <lokeshkandpal1208@gmail.com>
2025-09-23 22:27:21 +0530

    created server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express();
+
+
+app.use((req, res)=>{
+    res.send("hello from the server")
+})
+
+app.listen(3000 , ()=>{
+    console.log("Server is successfully running on PORT 3000");
+}); 
